23d02f9b0874e86327322f08a72f1299de88beab
jack-robs <jack.robertson@oneviewcommerce.com>
2020-04-26 20:56:36 -0400

    @property and @value.setter and tests done, which replaced get/setters

--- personGood.py
+++ personGood.py
@@ -2,7 +2,7 @@
 
 class personGood:
     
-    def __init__(self, personName, soloVal=None, modVal=None, trustedPartner=[]): 
+    def __init__(self, personName, soloVal=None, modVal=None):
         '''
         Fxn: __init__
         Does: init'd a personGood object, involved in shared priv key generation 
@@ -11,106 +11,95 @@ class personGood:
         - soloVal: `Y` in `Y^x (mod P)`, default of none, which allows one person to decide 
         and send `Y` value to the other personGood
         - modVal: `P` in `Y^x (modP)`, default of none, same reasoning
-        - trustedPartner: list strs, names that are trusted to key exchange with. 
-            - an object can only be instantiated w/ initial trusted partners  
-            - when a person wants to share with others, it will firts check if trusted, and then
-            add object.personName to registeredPartners
-            - all interactions after check if registeredPartner is ok
         '''
-
         # __ is private field
-        self.personName = personName
-        self.soloVal = soloVal
-        self.modVal = modVal
-        self.pubShare = None
-        # don't make a getter for __
+        self.__personName = personName
+        self.__soloVal = soloVal
+        self.__modVal = modVal
+        self.__pubShare = None
         self.__privKey = None
-        # TODO see if this makes sense in scheme of implied trust of sharing mods
-        self.__trustedPartner = trustedPartner
 
-    def __checkTrust(self, personClass):
-        #REMOVE __ FOR TESTING
+    
+    #-------------------@property getters---------------------#
+    
+    @property
+    def personName(self):
         '''
-        Fxn: checkTrust()
-        Does: confirms person object passed in is on trusted list
-        Params:
-        - personClass, object, personGood
-        Returns: bool, trust/no trust
-        '''
-        name = personClass.getName()
-        return name in self.__trustedPartner
-
-
-    def shareMod(self, modVal, personClass):
-        '''
-        Fxn: shareMod()
-        Does: sets modVal for personClass object 
-        Params: 
-        - modVal; int, modValue, `P` in `Y^x (mod P)`
-        - personClass: class, personGood object, other person in DHM key generation
-        Return: true/false boolean for success, handshake pattern
-        '''
-        
-        return True
-
-    def __setMod(self, modVal):
-        #TODO confirm only instance itself can set modVal
-        '''
-        Fxn: __setMod(), private function
-        Does: sets modVal if modVal cretaed by other partner
-        Params: modVal, value to set mod to. 
-        Return: none
-        '''
-        print("in class")
-        pass
-
-
-
-
-    def getName(self):
-        '''
-        Fxn: getName() 
-        Does: getter for personName
+        Fxn: @property personName()
+        Does: get personName
         Params: self
-        Return: self.personName
+        Returns: self._personName
         '''
-        return self.personName
+        return self.__personName
 
-    def getSoloVal(self):
+    @property
+    def soloVal(self):
         '''
-        Fxn: getSoloVal()
-        Does: getter soloVal
+        Fxn: @property soloVal()
+        Does: get soloVal
         Params: self
-        Return:  self.personName
+        Returns: self.__soloVal
         '''
-        return self.soloVal
+        return self.__soloVal
 
-    def getModVal(self):
+    @property
+    def modVal(self):
         '''
-        Fxn: getModVal()
-        Does: getter modVal
+        Fxn: @property modVal()
+        Does: get modVal
         Params: self
-        Return: self.modVal
+        Returns: self.__modVal
         '''
-        return self.ModVal
+        return self.__modVal
 
-    def getPubShare(self):
+    @property
+    def pubShare(self):
         '''
-        Fxn: getPubShare()
-        Does: getter pubShare
+        Fxn: @property pubShare()
+        Does: get pubShare
         Params: self
-        Return: self.pubShare
+        Returns: self.__pubShare
         '''
-        return self.pubShare
+        return self.__pubShare
 
-    def getPrivKey(self):
+ 
+    #-------------------@value.setter-----------------------#
+    @soloVal.setter
+    def soloVal(self, value):
         '''
-        Fxn: getPrivKey()
-        Does: getter privKey
-        Params: self
-        Return: self.privKey
+        Fxn: @x.setter soloVal()
+        Does: sets soloVal if other party decides on it and/or not instantaited with class
+        - soloVal must be less than modVal, if modVal already set
+        Params: self, value - value to set soloVal to in `soloVal^x (mod modVal)`
+        Returns: none
         '''
-        return self.privKey
+        if self.__modVal == None:
+            self.__soloVal = value
+        elif self.__modVal > value:
+            self.__soloVal = value
+        else:
+            raise ValueError("soloVal must be less than modVal, mod val already set")
+
+    @modVal.setter
+    def modVal(self, value):
+        '''
+        Fxn: @x.setter modVal()
+        Does: set modVal if other party decides on it and/or not instantiated with class
+        - modVal must be greater than soloVal, if soloVal already set
+        Params: self, value - value to set modVal to, int, `soloVal^x (mod modVal)`
+        Returns: none
+        '''
+        if self.__soloVal == None:
+            self.__modVal = value
+        elif self.__soloVal < value:
+            self.__modVal = value
+        else:
+            raise ValueError("modVal must be greater than soloVal, soloVal already set")
+
+
+
+    # TODO confirm privKey: do encryption, see if it works!
+
 
         
     def toString(self):
@@ -122,10 +111,10 @@ class personGood:
         '''
         #TODO add more fields for later steps
         person = ""
-        person += self.personName
-        person += " " + str(self.soloVal)
-        person += " " + str(self.modVal)
-        person += " " + str(self.pubShare)
+        person += self.__personName
+        person += " " + str(self.__soloVal)
+        person += " " + str(self.__modVal)
+        person += " " + str(self.__pubShare)
 
         return person
         
--- testDHM.py
+++ testDHM.py
@@ -57,106 +57,119 @@ def peopleBasicTest():
     except AssertionError as e: print(e)
     testRun += 1
 
-
     return passVal, testRun
 
-
-def peopleGettersTestInitial():
+def testGetters():
     '''
-    fxn: peopleGettersTest()
-    does: tests getters for personGood init fields
-    params: none
-    returns: passVal, int, 1 pass, 0 fail
-    '''
-    passVal = 0
-    testRun = 0
-     
-    pA = pg.personGood('jojo', 1, 2)
-
-
-    # personGood object, getName()
-    expA = pA.getName()
-    try:
-        assert expA == 'jojo', "Failed getName()"
-        passVal += 1
-    except AssertionError as e: print(e)
-    testRun += 1
-
-    # personGood object, getSoloVal()
-    expB = pA.getSoloVal()
-    try:
-        assert expB == 1, "Failed getSoloVal()"
-        passVal += 1
-    except AssertionError as e: print(e)
-    testRun += 1
-   
-
-    return passVal, testRun
-
-
-def checkTrustTest():
-    '''
-    fxn: checkTrustTest()
-    does: confirms personGood object that another personGood object interacts with
-    is on trusted list. trusted list is init'd with object
-    params: none
-    returns: passVal, testRun
-
-    NOTE: checkTrust is __ private in personGood, un-private for this test to run
+    Fxn: testGetters()
+    Does: test @property use for class fields
+    Params: none
+    returns passVal, testRun
     '''
     passVal, testRun = 0, 0
-    expA = pg.personGood('bob', 1, 2, ['josh', 'steven'])
-    expB = pg.personGood('josh')
-    expC  = pg.personGood('ryan')
-    
+
+    p0 = pg.personGood("bob", 1, 2)
+
     try:
-        # trusted
-        res1 = expA.checkTrust(expB)
-        try:
-            assert res1 == True, "Failed trusted check"
-            passVal += 1
-        except AssertionError as e: print(e)
-        testRun += 1
+        assert "bob" == p0.personName, "Failed @property personName"
+        passVal += 1
+    except AssertionError as e: print(e)
+    testRun += 1
 
-        # untrusted
-        res2 = expA.checkTrust(expC)
-        try: 
-            assert res2 == False, "Failed untrusted check"
-            passVal += 1
-        except AssertionError as e: print(e)
-        testRun += 1
-
-    except AttributeError:
-        print("\n\tcheckTrust() currently private in personGood(), make un-private for testing\n")
-        return 0, 0
-
-    #trusted
-
-    #not trusted
 
     return passVal, testRun
 
 
-
-
-def shareMod():
-    #TODO add to tests[]
+def testSetters():
     '''
-    fxn: setMod()
-    does: tests setting mod for key partner, and access modifiers
-    - want to prevent caller (or anyone else) from calling __setMod(), just share mode
+    fxn: testSetters()
+    Does: test @val.setter methods in personGood
     Params: none
     returns: passVal, testRun
     '''
-    passVal = 0
-    testRun = 0
+    passVal, testRun = 0, 0
 
-    expA = pg.personGood("alice", 1, 2, ["bob"])
-    expB = pg.personGood("bob")
+    # soloVal and modVal not set
+    p0 = pg.personGood("bob")
+    p0.soloVal = 1
+    p0.modVal =  2
+    try:
+        assert 1 == p0.soloVal, "Failed soloVal set, solo/mod None"
+        passVal += 1
+        assert 2 == p0.modVal, "Failed modVal set, solo/mod None, solo set first"
+        passVal += 1
+    except AssertionError as e: print(e)
+    testRun += 2
 
+    p1 = pg.personGood("joe")
+    p0.modVal = 2
+    p0.soloVal = 1
+    try:
+        assert 2 == p0.modVal, "Failed modVal set, solo/mod None, mod set first"
+        passVal += 1
+        assert 1 == p0.soloVal, "Failed soloVal set, solo/mod None, mod setFirst"
+        passVal += 1
+
+    except AssertionError as e: print(e)
+    testRun += 2
+
+    # soloVal set in instantiating, set modVal
+    p2 = pg.personGood("anna", 4)
+    p0.modVal = 5
+    try:
+        assert 5 == p0.modVal, "Failed modval set, solo set in instantiation"
+        passVal += 1
+    except AssertionError as e: print(e)
+    testRun += 1
+
+    # set soloVal, mod still none
+    p3 = pg.personGood("raul")
+    p3.soloVal = 6
+    try:
+        assert 6 == p3.soloVal, "Failed soloVal set, modVal none"
+        passVal +=1
+    except AssertionError as e: print(e)
+    testRun += 1
+
+    # set modVal, sol0 still none
+    p4 = pg.personGood("christine")
+    p4.soloVal = 3
+    try:
+        assert 3 == p4.soloVal, "Failed modVal set, soloVal none"
+        passVal += 1
+    except AssertionError as e: print(e)
+    testRun += 1
+
+    # modVal set in instantiating, set soloVal
+    p5 = pg.personGood("joejoe", modVal=5)
+    p5.soloVal = 4
+    try:
+        assert 4 == p5.soloVal, "Failed soloVal set, modVal set in inst."
+        passVal += 1
+    except AssertionError as e: print(e)
+    testRun += 1
     
+    # soloVal attempt, greater than mod, expect fail
+    p6 = pg.personGood("jojojojo", modVal=5)
+    try:
+        p6.soloVal = 6
+        print("Failed excepted soloVal > modVal logic deny")
+    except ValueError:
+        passVal += 1
+    testRun += 1
+
+    # mod val attempt, less than soloVal, expect fail
+    p7 = pg.personGood("hello", soloVal=2)
+    try:
+        p7.modVal = 1
+        print("Failed expected modVal < soloVal logic deny")
+    except ValueError:
+        passVal += 1
+    testRun += 1
+
 
     return passVal, testRun
+
 
 def main():
 
@@ -165,17 +178,17 @@ def main():
     testsRun = 0
     passVals = 0
 
+    tests = [peopleBasicTest, testGetters, testSetters]
     print("\n\t****Running Tests, in-test alerts:****\n")
-    tests = [peopleBasicTest(), peopleGettersTestInitial(), checkTrustTest()]
     for i in tests:
-        rval1, rval2 = i
+        rval1, rval2 = i()
         testsRun += rval2
         passVals += rval1
 
 
     percentPassed = str(passVals / testsRun * 100) + "%"
 
-    print("Tests run: " + str(testsRun))
+    print("\n\nTests run: " + str(testsRun))
     print("Tests passed: " + str(passVals))
     print("% passed: " + percentPassed)
 
